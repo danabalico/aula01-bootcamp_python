585d8d6835a058c8af178da15f48a5e29b309da7
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-06 14:51:07 -0300

    exemplo 02 da aula 2 - concatenação

---
+++ exemplo02.py
@@ -0,0 +1,5 @@
+#2 - Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(int(input("Digite um valor: ")) + int(input("Digite outro valor: ")))
+numero = 3 #int
+print(type(numero))
